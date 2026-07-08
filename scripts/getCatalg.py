f3e248437a35568dbf110ea2516d844c4a1fad72
Andoni Salcedo Navarro <andonisalcedo@gmail.com>
2026-07-08 21:41:06 +0200

    chore: update nutritional database to 1419 items and refactor food data handling

--- scripts/getCatalg.py
+++ scripts/getCatalg.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # 1. Leer el archivo Excel
 # Asegúrate de que el nombre del archivo coincida exactamente con el tuyo
-nombre_archivo = 'excel_pro_definitivo_base_datos_nutricional_C_1200_alimentos.xlsx'
+nombre_archivo = 'base_datos_nutricional_ampliada_1419_alimentos.xlsx'
 
 try:
     # Leemos específicamente la hoja 'alimentos_master'
